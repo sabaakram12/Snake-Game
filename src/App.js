9c9acb8eabc84896dd97dab3110e20261c886191
sabaakram12 <168063496+sabaakram12@users.noreply.github.com>
2024-06-12 23:03:43 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+
+
+import GameState from './GameState';
+
+
+
+
+import './App.css';
+
+function App() {
+  return (
+  <div className='container'>
+    <GameState/>
+  </div>
+  );
+}
+
+export default App;
